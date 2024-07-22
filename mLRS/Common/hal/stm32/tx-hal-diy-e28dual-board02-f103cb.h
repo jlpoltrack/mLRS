bb731d9645ea0197721c32fa0cd8174a61d02ef0
JLP <jlpoltrack@gmail.com>
2024-07-22 19:50:13 -0400

    YakE22

--- mLRS/Common/hal/stm32/tx-hal-diy-e28dual-board02-f103cb.h
+++ mLRS/Common/hal/stm32/tx-hal-diy-e28dual-board02-f103cb.h
@@ -7,36 +7,11 @@
 // hal
 //********************************************************
 
-//#define MLRS_FEATURE_JRPIN5
-//#define MLRS_FEATURE_IN
-//#define MLRS_FEATURE_DIVERSITY
-//#define MLRS_FEATURE_NO_DIVERSITY
-
 //-------------------------------------------------------
-// TX DIY DUAL-E28 BOARD02 v010 STM32F103CB
+// TX DIY E22 400 YAK STM32F103CB
 //-------------------------------------------------------
 
-#define DEVICE_HAS_DIVERSITY
-#define DEVICE_HAS_JRPIN5 // requires diode from Tx to Rx soldered on the board
-//#define DEVICE_HAS_IN
-#define DEVICE_HAS_DEBUG_SWUART
-
-
-#ifdef MLRS_FEATURE_JRPIN5
-  #undef DEVICE_HAS_IN
-  #define DEVICE_HAS_JRPIN5
-#endif
-#ifdef MLRS_FEATURE_IN
-  #undef DEVICE_HAS_JRPIN5
-  #define DEVICE_HAS_IN
-#endif
-#ifdef MLRS_FEATURE_DIVERSITY
-  #define DEVICE_HAS_DIVERSITY
-#endif
-#ifdef MLRS_FEATURE_NO_DIVERSITY
-  #undef DEVICE_HAS_DIVERSITY
-#endif
-
+#define DEVICE_HAS_NO_DEBUG
 
 //-- Timers, Timing, EEPROM, and such stuff
 
@@ -69,40 +44,6 @@
 #define UARTC_USE_RX
 #define UARTC_RXBUFSIZE           TX_COM_RXBUFSIZE
 
-#ifdef DEVICE_HAS_IN
-#define UARTE_USE_UART2_PA2PA3 // in pin
-#define UARTE_BAUD                100000 // SBus normal baud rate, is being set later anyhow
-//#define UARTE_USE_TX
-//#define UARTE_TXBUFSIZE           512
-//#define UARTE_USE_TX_ISR
-#define UARTE_USE_RX
-#define UARTE_RXBUFSIZE           512
-#endif
-#ifdef DEVICE_HAS_JRPIN5
-#define UART_USE_UART2_PA2PA3 // JR pin5, MBridge
-#define UART_BAUD                 400000
-#define UART_USE_TX
-#define UART_TXBUFSIZE            512
-#define UART_USE_TX_ISR
-#define UART_USE_RX
-#define UART_RXBUFSIZE            512
-
-#define JRPIN5_TX_XOR             IO_PB9
-#define JRPIN5_TX_SET_NORMAL      gpio_low(JRPIN5_TX_XOR)
-#define JRPIN5_TX_SET_INVERTED    gpio_high(JRPIN5_TX_XOR)
-#define JRPIN5_RX_XOR             IO_PC15
-#define JRPIN5_RX_SET_NORMAL      gpio_low(JRPIN5_RX_XOR)
-#define JRPIN5_RX_SET_INVERTED    gpio_high(JRPIN5_RX_XOR)
-#endif
-
-#define SWUART_USE_TIM2 // debug
-#define SWUART_TX_IO              IO_PB7 // is the BUZZER/EXT pad
-#define SWUART_BAUD               115200
-#define SWUART_USE_TX
-#define SWUART_TXBUFSIZE          512
-//#define SWUART_TIM_IRQ_PRIORITY   9
-
-
 //-- SX1: SX12xx & SPI
 
 #define SPI_USE_SPI1 // PA5, PA6, PA7
@@ -111,8 +52,8 @@
 #define SPI_USE_CLOCKSPEED_9MHZ
 
 #define SX_RESET                  IO_PB5
-#define SX_DIO1                   IO_PB3
-#define SX_BUSY                   IO_PB4
+#define SX_DIO1                   IO_PB2
+#define SX_BUSY                   IO_PB1
 #define SX_RX_EN                  IO_PB8
 #define SX_TX_EN                  IO_PB12
 
@@ -175,102 +116,9 @@ void sx_dio_exti_isr_clearflag(void)
 }
 
 
-//-- SX12xx II & SPIB
-
-#define SPIB_USE_SPI2             // PB13, PB14, PB15
-#define SPIB_CS_IO                IO_PA8
-#define SPIB_USE_CLK_LOW_1EDGE    // datasheet says CPHA = 0  CPOL = 0
-#define SPIB_USE_CLOCKSPEED_9MHZ
-
-#define SX2_RESET                 IO_PA4
-#define SX2_DIO1                  IO_PA1
-#define SX2_BUSY                  IO_PA0
-#define SX2_RX_EN                 IO_PB6
-#define SX2_TX_EN                 IO_PB1
-
-#define SX2_DIO1_GPIO_AF_EXTI_PORTx   LL_GPIO_AF_EXTI_PORTA
-#define SX2_DIO1_GPIO_AF_EXTI_LINEx   LL_GPIO_AF_EXTI_LINE1
-#define SX2_DIO_EXTI_LINE_x           LL_EXTI_LINE_1
-#define SX2_DIO_EXTI_IRQn             EXTI1_IRQn
-#define SX2_DIO_EXTI_IRQHandler       EXTI1_IRQHandler
-//#define SX2_DIO_EXTI_IRQ_PRIORITY   11
-
-void sx2_init_gpio(void)
-{
-    gpio_init(SX2_RESET, IO_MODE_OUTPUT_PP_HIGH, IO_SPEED_VERYFAST);
-    gpio_init(SX2_DIO1, IO_MODE_INPUT_PD, IO_SPEED_VERYFAST);
-    gpio_init(SX2_BUSY, IO_MODE_INPUT_PU, IO_SPEED_VERYFAST);
-    gpio_init(SX2_TX_EN, IO_MODE_OUTPUT_PP_LOW, IO_SPEED_VERYFAST);
-    gpio_init(SX2_RX_EN, IO_MODE_OUTPUT_PP_LOW, IO_SPEED_VERYFAST);
-}
-
-bool sx2_busy_read(void)
-{
-    return (gpio_read_activehigh(SX2_BUSY)) ? true : false;
-}
-
-void sx2_amp_transmit(void)
-{
-    gpio_low(SX2_RX_EN);
-    gpio_high(SX2_TX_EN);
-}
-
-void sx2_amp_receive(void)
-{
-    gpio_low(SX2_TX_EN);
-    gpio_high(SX2_RX_EN);
-}
-
-void sx2_dio_init_exti_isroff(void)
-{
-    LL_GPIO_AF_SetEXTISource(SX2_DIO1_GPIO_AF_EXTI_PORTx, SX2_DIO1_GPIO_AF_EXTI_LINEx);
-
-    // let's not use LL_EXTI_Init(), but let's do it by hand, is easier to allow enabling isr later
-    LL_EXTI_DisableEvent_0_31(SX2_DIO_EXTI_LINE_x);
-    LL_EXTI_DisableIT_0_31(SX2_DIO_EXTI_LINE_x);
-    LL_EXTI_DisableFallingTrig_0_31(SX2_DIO_EXTI_LINE_x);
-    LL_EXTI_EnableRisingTrig_0_31(SX2_DIO_EXTI_LINE_x);
-
-    NVIC_SetPriority(SX2_DIO_EXTI_IRQn, SX2_DIO_EXTI_IRQ_PRIORITY);
-    NVIC_EnableIRQ(SX2_DIO_EXTI_IRQn);
-}
-
-void sx2_dio_enable_exti_isr(void)
-{
-    LL_EXTI_ClearFlag_0_31(SX2_DIO_EXTI_LINE_x);
-    LL_EXTI_EnableIT_0_31(SX2_DIO_EXTI_LINE_x);
-}
-
-void sx2_dio_exti_isr_clearflag(void)
-{
-    LL_EXTI_ClearFlag_0_31(SX2_DIO_EXTI_LINE_x);
-}
-
-
-//-- In port
-
-#define IN_XOR                    IO_PC15
-
-void in_init_gpio(void)
-{
-    gpio_init(IN_XOR, IO_MODE_OUTPUT_PP_LOW, IO_SPEED_VERYFAST);
-    gpio_low(IN_XOR);
-}
-
-void in_set_normal(void)
-{
-    gpio_low(IN_XOR);
-}
-
-void in_set_inverted(void)
-{
-    gpio_high(IN_XOR);
-}
-
-
 //-- Button
 
-#define BUTTON                    IO_PA15
+#define BUTTON                    IO_PC13
 
 void button_init(void)
 {
@@ -285,8 +133,8 @@ bool button_pressed(void)
 
 //-- LEDs
 
-#define LED_GREEN 		          IO_PC14
-#define LED_RED		              IO_PC13
+#define LED_GREEN 		          IO_PA11
+#define LED_RED		              IO_PA12
 
 void leds_init(void)
 {
@@ -307,8 +155,18 @@ void led_red_toggle(void) { gpio_toggle(LED_RED); }
 
 //-- POWER
 
-#define POWER_PA_E28_2G4M27SX
-#include "../hal-power-pa.h"
+#define POWER_GAIN_DBM            13 // gain of a PA stage if present
+#define POWER_SX126X_MAX_DBM      SX126X_POWER_MAX // maximum allowed sx power
+#define POWER_USE_DEFAULT_RFPOWER_CALC
+
+#define RFPOWER_DEFAULT           1 // index into rfpower_list array
+
+const rfpower_t rfpower_list[] = {
+    { .dbm = POWER_MIN, .mW = INT8_MIN },
+    { .dbm = POWER_20_DBM, .mW = 100 },
+	{ .dbm = POWER_24_DBM, .mW = 250 },
+	{ .dbm = POWER_30_DBM, .mW = 1000 },
+};
 
 
 //-- TEST
